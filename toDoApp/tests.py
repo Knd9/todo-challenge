d30fbd887f974791dfdbf2495f4f0f132ea51554
Knd9 <candela.fiume@gmail.com>
2021-01-14 19:13:21 -0300

    autopep8, tests for Create a ToDo, add test_serializer.py, change API response

--- toDoApp/tests.py
+++ toDoApp/tests.py
@@ -1,65 +1,123 @@
 import pytest
 from rest_framework.test import APIClient, APIRequestFactory, APITestCase
 from rest_framework import status
-from django.urls import reverse
-from toDoApp.models import ToDo
-from toDoApp.views import ToDoViewSet
+from toDoApp import models as md
+from toDoApp import views as vw
 
 
 # to integrational tests
 @pytest.fixture
 def api_client():
-   return APIClient()
+   return APIClient(enforce_csrf_checks=True)
+
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
    'title, description, status_code', [
-        (None, None, 400),
-        ('', None, 400),
-        (None, '', 400),
-        ('', '', 400),
+        (None, None, status.HTTP_400_BAD_REQUEST),
+        ('', None, status.HTTP_400_BAD_REQUEST),
+        (None, '', status.HTTP_400_BAD_REQUEST),
+        ('', '', status.HTTP_400_BAD_REQUEST),
         (
         """
         This message will be write 3 times.
         This message will be write 3 times.
         This message will be write 3 times.
-        """, '', 400),
-        (800, '', 201),
-        ('hello', 12, 201),
-        ('hello', '', 201)
-
+        """, '', status.HTTP_400_BAD_REQUEST),
+        (800, '', status.HTTP_201_CREATED),
+        ('hello', 12, status.HTTP_201_CREATED),
+        ('hello', '', status.HTTP_201_CREATED),
+        ('hello', 'description', status.HTTP_201_CREATED)
    ]
 )
-
-
 def test_create_todo(title, description, status_code, api_client):
     url = '/todos/'
     data = {
-        'title': title, 
-        'description': description,
+        'title': title,
+        'description': description
     }
     response = api_client.post(url, data=data, format='json')
     print(response.data)
     assert response.status_code == status_code
+    assert(md.ToDo.objects.count(), 1)
 
-"""
+
 class ToDoTests(APITestCase):
-    def test_create_todo_OK(self):
-        
+        """
         Ensure we can create a new todo object.
-        
-        data = {'title': 'new idea'}
-        response = self.client.post(url, data, format='json')
+        """
+    def test_create_todo_OK(self):
+        data = {
+            'title': 'todo title', 
+            'description': '',
+        }
+        response = self.client.post('/todos/', data, format='json')
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(ToDo.objects.count(), 1)
-        self.assertEqual(ToDo.objects.get().title, 'new idea')
-"""
-"""
-# to unit tests
+        self.assertEqual(md.ToDo.objects.count(), 1)
+        self.assertEqual(md.ToDo.objects.get().title, 'todo title')
+        self.assertEqual(md.ToDo.objects.get().description, '')
+
+    def test_create_todo_int_title(self):
+        data = {
+            'title': 800, 
+            'description': '',
+        }
+        response = self.client.post('/todos/', data, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(md.ToDo.objects.count(), 1)
+        self.assertEqual(md.ToDo.objects.get().title, '800')
+        self.assertEqual(md.ToDo.objects.get().description, '')
+
+    def test_create_todo_int_description(self):
+        data = {
+            'title': 'hello', 
+            'description': 12,
+        }
+        response = self.client.post('/todos/', data, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(md.ToDo.objects.count(), 1)
+        self.assertEqual(md.ToDo.objects.get().title, 'hello')
+        self.assertEqual(md.ToDo.objects.get().description, '12')
+
+
+# unit tests for views
 @pytest.fixture
-factory = APIRequestFactory()
+def api_factory():
+    return APIRequestFactory()
 
 
-request = factory.post('/todos/', {'title': 'new idea'}, format='json')
-assert request.status_code == 201
-"""
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+   'title, description, status_code', [
+        (None, None, status.HTTP_400_BAD_REQUEST),
+        ('', None, status.HTTP_400_BAD_REQUEST),
+        (None, '', status.HTTP_400_BAD_REQUEST),
+        ('', '', status.HTTP_400_BAD_REQUEST),
+        (
+        """
+        This message will be write 3 times.
+        This message will be write 3 times.
+        This message will be write 3 times.
+        """, '', status.HTTP_400_BAD_REQUEST),
+        (800, '', status.HTTP_201_CREATED),
+        ('hello', 12, status.HTTP_201_CREATED),
+        ('hello', '', status.HTTP_201_CREATED),
+        ('hello', 'description', status.HTTP_201_CREATED)
+   ]
+)
+
+def test_create_todo_factory(title, description, status_code, api_factory):
+    data = { 
+        'title': title, 
+        'description': description 
+    }
+    request = api_factory.post('/todos/', data=data, format='json')
+    view = vw.ToDoViewSet.as_view({ 'post': 'create' })
+    response = view(request)
+    print("view\n")
+    print(response)
+    assert(response.status_code, status_code)
+    assert(md.ToDo.objects.count(), 1)
